48a183e0d776f07d765bde111476d7d1c3fa3def
tekthree <tekthree.gmail.com>
2021-07-19 21:54:08 -0700

    add functional components

---
+++ src/components/results/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './results.scss';
+
+function Results (props) {
+  
+    return (
+      <section>
+        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      </section>
+    );
+ 
+}
+
+export default Results;
